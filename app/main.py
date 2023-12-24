02f2ebeda049aff7bb93ae56fc7c73f05e4793cd
Vugar <messi07070@gmail.com>
2023-12-24 22:49:16 +0300

    user model, config, alembic

---
+++ app/main.py
@@ -0,0 +1,27 @@
+# from fastapi import FastAPI
+# import uvicorn
+# from fastapi.routing import APIRouter
+
+# #########################
+# # BLOCK WITH API ROUTES #
+# #########################
+
+# # create instance of the app
+# app = FastAPI()
+
+# # create the instance for the routes
+# main_api_router = APIRouter()
+
+# # # set routes to the app instance
+# # main_api_router.include_router(user_router, prefix="/user", tags=["user"])
+# # app.include_router(main_api_router)
+
+
+# @app.get("/")
+# async def root():
+#     return {"message": "Hello World"}
+
+
+# if __name__ == "__main__":
+#     # run app on the host and port
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
